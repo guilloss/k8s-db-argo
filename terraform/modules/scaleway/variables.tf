91897603961b62fa95a926eb81eb7cef1b1d7fae
Olivier Guillossou <olivier@guillossou.fr>
2025-05-24 16:49:13 +0200

    ajout vars

--- terraform/modules/scaleway/variables.tf
+++ terraform/modules/scaleway/variables.tf
@@ -17,3 +17,15 @@ variable "k8s_pvn_name" {
     type = string
     default = "pvn-k8s-db"
 }
+variable "k8s_scw_node_type" {
+    type = string
+    default = "DEV1-M"
+}
+variable "scw_region" {
+    type = string
+    default = "fr-par"
+}
+variable "scw_zone" {
+    type = string
+    default = "fr-par-1"
+}
